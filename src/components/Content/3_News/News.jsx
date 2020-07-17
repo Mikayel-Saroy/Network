887cb682ee162cb95a01b5a1a50bc5ff8695b97e
Mikayel Saroyan <mikayelsaroyan@Mikayels-MacBook-Pro.local>
2020-07-17 10:35:42 +0400

    Test

--- src/components/Content/3_News/News.jsx
+++ src/components/Content/3_News/News.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import st from './News.module.css';
+import NewsInfo from "./NewsInfo/NewsInfo";
 
 const News = (props) => {
     return (
         <div className={st.main}>
             <h1>News</h1>
+            <NewsInfo info="Here we will post news." />
         </div>
     )
 }
---
+++ src/components/Content/3_News/NewsInfo/NewsInfo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewsInfo = (props) => {
+    return (
+        <div>
+            <p>{props.info}</p>
+        </div>
+    )
+}
+
+export default NewsInfo;
