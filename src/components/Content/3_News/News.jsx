3067e18f2ed83df6d6b4cf16bc9c606ee8d48639
Mikayel Saroyan <mikayelsaroyan@Mikayels-MacBook-Pro.local>
2020-07-17 10:37:44 +0400

    Test

--- src/components/Content/3_News/News.jsx
+++ src/components/Content/3_News/News.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import st from './News.module.css';
-import NewsInfo from "./NewsInfo/NewsInfo";
 
 const News = (props) => {
     return (
         <div className={st.main}>
             <h1>News</h1>
-            <NewsInfo info="Here we will post news." />
         </div>
     )
 }
